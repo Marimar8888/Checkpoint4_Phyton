eafd26f34229ad3369838dad05788467dc5bfb60
Marimar8888 <alonsomarimar@hotmail.com>
2024-02-20 10:45:44 +0100

    Exercise 1

---
+++ checkponit4.py
@@ -0,0 +1,21 @@
+from decimal import Decimal
+
+# Exercise 1
+
+list_name = ['Maria', 'Sara', 'Luis', 'Alberto', 'Lidia']
+
+tupla_book = ('English Intermediate', 'John Milne', 'The Queen')
+
+float_one = 47.20
+
+int_one = 22
+
+dict_players = {
+    '6': 'LeBron James',
+    '8': 'Kobe Bryan',
+    '23': 'Michael Jordan',
+    '32': 'Magic Johnson',
+    '33': 'Larry Bird',
+}
+
+service_price = Decimal(12.99)
